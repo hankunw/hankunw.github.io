38175105de64c2002aa7f1b4c947a8baa9714389
Haokfu <mikewang243@gmail.com>
2025-05-02 10:04:46 -0400

    projects bug fix

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'reports', 'awards']
+const section_names = ['home', 'projects', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
